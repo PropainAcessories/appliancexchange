c0c688bc548483d659c0dc5a93e5e8d5b99024e7
HankHill <hhc3h8@gmail.com>
2022-12-07 20:15:20 -0600

    another useful comment for myself

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,7 @@
+const { gql } = require('apollo-server-express');
+// Start to fill this out AFTER you finish the models otherwise you will fucking regret it.
+const typeDefs = gql`
+
+`;
+
+module.exports = typeDefs;
