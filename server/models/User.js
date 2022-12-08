98f6bc00d5e4dfc02a2d435723d70fda77e28f36
HankHill <hhc3h8@gmail.com>
2022-12-08 07:50:40 -0600

    another model

---
+++ server/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+const Order = require('./Order');
+// Import stuff they're selling to when you get to it.
+// Add other constraints to password later.
+// Payment stuff likely goes here
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    orders: [Order.schema]
+});
+
+userSchema.pre('save', async function(next) {
+    if(this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hashSync(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function(password) {
+    return await bcrypt.compareSync(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
