b0d7243c55b265da232603c6aec232eceff6531e
HankHill <hhc3h8@gmail.com>
2022-12-07 20:23:10 -0600

    finished one model (the easiest one probably)

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,39 @@
 const { gql } = require('apollo-server-express');
 // Start to fill this out AFTER you finish the models otherwise you will fucking regret it.
 const typeDefs = gql`
+  type Category {
+    _id: ID!
+    name: String!
+  }
 
+  type Product {
+
+  }
+
+  type Order {
+
+  }
+
+  type User {
+
+  }
+
+  type Checkout {
+    
+  }
+
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+
+  }
+
+  type Mutation {
+
+  }
 `;
 
 module.exports = typeDefs;
