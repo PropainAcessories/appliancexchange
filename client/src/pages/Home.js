e4404133a0a2633d734a64dd74be776056f948cc
HankHill <hhc3h8@gmail.com>
2022-12-12 08:11:48 -0600

    pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Category from '../components/Categories';
+import Products from '../components/Products';
+import Cart from '../components/Cart';
 
 function Home() {
     return (
         <div className="container">
             <Category />
+            <Products />
+            <Cart />
         </div>
     );
 };
