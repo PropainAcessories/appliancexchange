56c7eb39d1bd500e40efd5358d87f3bfe46cd201
HankHill <hhc3h8@gmail.com>
2022-12-12 19:51:43 -0600

    comment

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Category from '../components/Categories';
 import Products from '../components/Products';
 import Cart from '../components/Cart';
-
+// PUT CART SOMEWHERE ELSE IT LOOKS FUCKING STUPID AND MAKES YOU LOOK STUPID AND YOU SHOULD FEEL STUPID!
 function Home() {
     return (
         <div className="container">
