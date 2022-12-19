a8e20e3c9bae28a05e21f558a81c60be3357133d
HankHill <hhc3h8@gmail.com>
2022-12-19 12:25:15 -0600

    Revert "AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH"
    This reverts commit 5cc69b95149476317f421ab6cafd21ad1c093d6c.

--- client/src/components/sellItem/index.js
+++ client/src/components/sellItem/index.js
@@ -3,12 +3,11 @@ import { useMutation } from '@apollo/client';
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_PRODUCT } from '../../utils/mutations';
 import { NavLink } from 'react-router-dom';
-import { QUERY_CATEGORIES, QUERY_USER } from '../../utils/queries';
+import { QUERY_CATEGORIES } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
 import { idbPromise } from '../../utils/helpers';
 import {
     UPDATE_CATEGORIES,
-    UPDATE_USER
 } from '../../utils/actions';
 
 
@@ -18,16 +17,10 @@ function SellItem() {
 
     const { categories } = state;
 
-    const { userData } = useQuery(QUERY_USER);
 
     const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
     useEffect(() => {
-        if (userData) {
-            dispatch({
-                type: UPDATE_USER,
-                user: userData.UPDATE_USER
-            })
-        }
+
         if(categoryData) {
             dispatch({
                 type: UPDATE_CATEGORIES,
@@ -44,7 +37,7 @@ function SellItem() {
                 });
             });
         }
-    }, [categoryData, userData, loading, dispatch]);
+    }, [categoryData, loading, dispatch]);
 
     const [formState, setFormState] = useState({ category: '', name: '', description: '', price: '', quantity: '', image: '' })
     const [addProduct] = useMutation(ADD_PRODUCT);
