8202be20593022f1f008c677b7dda1eb8bf3dc01
HankHill <hhc3h8@gmail.com>
2022-12-25 22:15:14 -0600

    deploy connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/appliancexchange',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
